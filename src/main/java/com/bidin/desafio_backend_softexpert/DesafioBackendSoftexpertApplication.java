1d9a41f6202a3970b2a020dcac15478a00f58a65
bidinpithecus <vinibidin@gmail.com>
2025-01-20 20:20:06 -0300

    project setup

---
+++ src/main/java/com/bidin/desafio_backend_softexpert/DesafioBackendSoftexpertApplication.java
@@ -0,0 +1,13 @@
+package com.bidin.desafio_backend_softexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackendSoftexpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackendSoftexpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidin/desafio_backend_softexpert/DesafioBackendSoftexpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidin.desafio_backend_softexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendSoftexpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
