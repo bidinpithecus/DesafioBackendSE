70dfc31f31837440a1441d5b21257f49abe7ea22
bidinpithecus <vinibidin@gmail.com>
2025-01-22 17:24:33 -0300

    order controller done

--- src/main/java/com/bidin/desafio_backend_softexpert/controller/OrderController.java
+++ src/main/java/com/bidin/desafio_backend_softexpert/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.bidin.desafio_backend_softexpert.controller;
 
+import com.bidin.desafio_backend_softexpert.dto.OrderRequestDTO;
+import com.bidin.desafio_backend_softexpert.dto.OrderResponseDTO;
+import com.bidin.desafio_backend_softexpert.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/split")
+    public ResponseEntity<OrderResponseDTO> split(@RequestBody OrderRequestDTO request) {
+        try {
+            return new ResponseEntity<>(orderService.split(request), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
