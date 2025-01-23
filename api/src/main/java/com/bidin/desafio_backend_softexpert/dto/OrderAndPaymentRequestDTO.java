147c39a3feb63a710d036ecc1d29761ddb1bc52b
bidinpithecus <vinibidin@gmail.com>
2025-01-22 23:00:25 -0300

    better request validations

--- api/src/main/java/com/bidin/desafio_backend_softexpert/dto/OrderAndPaymentRequestDTO.java
+++ api/src/main/java/com/bidin/desafio_backend_softexpert/dto/OrderAndPaymentRequestDTO.java
@@ -3,15 +3,30 @@ package com.bidin.desafio_backend_softexpert.dto;
 import com.bidin.desafio_backend_softexpert.model.Addition;
 import com.bidin.desafio_backend_softexpert.model.Discount;
 import com.bidin.desafio_backend_softexpert.model.Item;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.Map;
 
 public class OrderAndPaymentRequestDTO {
-    private final Map<String, List<Item>> items;
+    @NotNull(message = "Itens não pode ser vazio")
+    @NotEmpty(message = "Itens não pode ser vazio")
+    @Valid
+    private final Map<String, @Valid List<@Valid Item>> items;
+    @NotNull(message = "Descontos não pode ser vazio")
+    @NotEmpty(message = "Descontos não pode ser vazio")
     private final List<Discount> discounts;
+    @NotNull(message = "Adicionais não pode ser vazio")
+    @NotEmpty(message = "Adicionais não pode ser vazio")
     private final List<Addition> additions;
+    @NotNull(message = "Nome do proprietário não pode ser vazio")
+    @NotBlank(message = "Nome do proprietário não pode ser em vazio")
     private final String ownerName;
+    @NotNull(message = "Chave PIX do proprietário não pode ser vazio")
+    @NotBlank(message = "Chave PIX do proprietário não pode ser em vazio")
     private final String ownerPixKey;
 
     public OrderAndPaymentRequestDTO(Map<String, List<Item>> items, List<Discount> discounts, List<Addition> additions, String ownerName, String ownerPixKey) {
--- api/src/main/java/com/bidin/desafio_backend_softexpert/dto/PaymentRequestDTO.java
+++ api/src/main/java/com/bidin/desafio_backend_softexpert/dto/PaymentRequestDTO.java
@@ -1,10 +1,23 @@
 package com.bidin.desafio_backend_softexpert.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.util.Map;
 
 public class PaymentRequestDTO {
-    private final Map<String, Double> shares;
+    @NotNull(message = "Itens não pode ser vazio")
+    @NotEmpty(message = "Itens não pode ser vazio")
+    @Valid
+    private final Map<@NotBlank String, @Positive Double> shares;
+    @NotNull(message = "Nome do proprietário não pode ser vazio")
+    @NotBlank(message = "Nome do proprietário não pode ser em vazio")
     private final String ownerName;
+    @NotNull(message = "Chave PIX do proprietário não pode ser vazio")
+    @NotBlank(message = "Chave PIX do proprietário não pode ser em vazio")
     private final String ownerPixKey;
 
     public PaymentRequestDTO(Map<String, Double> shares, String ownerName, String ownerPixKey) {
